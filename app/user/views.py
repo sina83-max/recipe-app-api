30dc05fa3b8cfd225d3ba1356ffceead7d4ba2cb
Sina <s.beignazary1383@gmail.com>
2025-03-27 10:54:26 +0330

    Create and write test for token authentication

--- app/user/views.py
+++ app/user/views.py
@@ -2,10 +2,21 @@
 Views for the user API
 """
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from .serializers import UserSerializer
+from .serializers import (
+    UserSerializer,
+    AuthTokenSerializers,
+)
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create token for signed-in users"""
+    serializer_class = AuthTokenSerializers
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
